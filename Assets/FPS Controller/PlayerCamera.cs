2e9850d83adbbeba7c9fc0f64bd028a7ed6eaaf9
Liam Cohron <therealkiro0613@gmail.com>
2019-08-04 22:19:24 -0400

    Initial commit

---
+++ Assets/FPS Controller/PlayerCamera.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasySurvivalScripts {
+    public enum CameraPerspective {
+        FirstPerson,
+        ThirdPerson
+    }
+
+    public class PlayerCamera : MonoBehaviour {
+
+        [Header("Input Settings")]
+        public string MouseXInput;
+        public string MouseYInput;
+
+        [Header("Common Camera Settings")]
+        public float mouseSensitivity;
+        public CameraPerspective cameraPerspective;
+
+        [Header("FPS Camera Settings")]
+        public Vector3 FPS_CameraOffset;
+        public Vector2 FPS_MinMaxAngles;
+
+        [Header("TPS Camera Settings")]
+        public Vector3 TPS_CameraOffset;
+        public Vector2 TPS_MinMaxAngles;
+
+        [Header("Leaning")]
+        public string leanInput = "Lean";
+        public float zInput;
+        public float leanSpeed;
+        public float leanAngle;
+
+        Transform FPSController;
+        float xClamp;
+        Vector3 camMoveLoc;
+        Transform _fpsCameraHelper;
+        Transform _tpsCameraHelper;
+
+        private void Awake() {
+            Cursor.lockState = CursorLockMode.Locked;
+            xClamp = 0;
+            FPSController = GetComponentInParent<PlayerMovement>().transform;
+        }
+
+        // Use this for initialization
+        void Start() {
+        }
+
+        void Add_FPSCamPositionHelper() {
+            _fpsCameraHelper = new GameObject().transform;
+            _fpsCameraHelper.name = "_fpsCameraHelper";
+            _fpsCameraHelper.localPosition = Vector3.zero;
+        }
+
+
+        void Add_TPSCamPositionHelper() {
+            _tpsCameraHelper = new GameObject().transform;
+            _tpsCameraHelper.name = "_tpsCameraHelper";
+            _tpsCameraHelper.SetParent(FPSController);
+            _tpsCameraHelper.localPosition = Vector3.zero;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            RotateCamera();
+
+            zInput = Input.GetAxisRaw(leanInput);
+            float step = leanSpeed * Time.deltaTime;
+            transform.localEulerAngles = Vector3.MoveTowards(transform.localEulerAngles, new Vector3(0f, 0f, leanAngle * zInput), step);
+        }
+
+        void RotateCamera() {
+            float step = leanSpeed * Time.deltaTime;
+
+            float mouseX = Input.GetAxis(MouseXInput) * (mouseSensitivity * Time.deltaTime);
+            float mouseY = Input.GetAxis(MouseYInput) * (mouseSensitivity * Time.deltaTime);
+            Vector3 eulerRotation = transform.eulerAngles;
+
+            xClamp += mouseY;
+
+            if(cameraPerspective == CameraPerspective.FirstPerson) {
+                xClamp = Mathf.Clamp(xClamp, FPS_MinMaxAngles.x, FPS_MinMaxAngles.y);
+            } else {
+                xClamp = Mathf.Clamp(xClamp, TPS_MinMaxAngles.x, TPS_MinMaxAngles.y);
+            }
+
+            eulerRotation.x = -xClamp;
+            transform.eulerAngles = eulerRotation;
+            FPSController.Rotate(Vector3.up * mouseX);
+        }
+
+        private void OnDrawGizmosSelected() {
+            if(_fpsCameraHelper) {
+                Gizmos.DrawWireSphere(_fpsCameraHelper.position, 0.1f);
+            }
+
+            Gizmos.color = Color.green;
+
+            if(_tpsCameraHelper) {
+                Gizmos.DrawWireSphere(_tpsCameraHelper.position, 0.1f);
+            }
+        }
+
+        private void lean() {
+
+        }
+    }
+}
---
+++ Assets/FPS Controller/PlayerMovement.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasySurvivalScripts {
+    public enum PlayerStates {
+        Idle,
+        Walking,
+        Running,
+        Jumping
+    }
+
+    public class PlayerMovement : MonoBehaviour {
+        public PlayerStates playerStates;
+
+        public bool canMove;
+
+        [Header("Inputs")]
+        public string HorizontalInput = "Horizontal";
+        public string VerticalInput = "Vertical";
+        public string RunInput = "Run";
+        public string JumpInput = "Jump";
+
+        [Header("Player Motor")]
+        [Range(1f, 15f)]
+        public float walkSpeed;
+        [Range(1f, 15f)]
+        public float runSpeed;
+        [Range(1f, 15f)]
+        public float JumpForce;
+
+        [Header("Audio")]
+        public AudioClip footstepSound;
+        public AudioClip landingSound;
+        private AudioSource audioSource;
+        private int stepCycle;
+        private int stepCycleSize;
+        private bool incrementStepCycle;
+        private int nextStep;
+        public int nextStepIncrement;
+        private bool didJump;
+
+        CharacterController characterController;
+
+        // Use this for initialization
+        void Start() {
+            characterController = GetComponent<CharacterController>();
+            audioSource = GetComponent<AudioSource>();
+            nextStepIncrement = 60;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            //handle controller
+            HandlePlayerControls();
+        }
+
+        private void FixedUpdate() {
+            if(incrementStepCycle) {
+                stepCycle += stepCycleSize;
+                if(stepCycle >= nextStep) {
+                    playSound(footstepSound);
+                    nextStep += nextStepIncrement;
+                }
+            }
+        }
+
+        void HandlePlayerControls() {
+            float hInput = Input.GetAxisRaw(HorizontalInput);
+            float vInput = Input.GetAxisRaw(VerticalInput);
+
+            Vector3 fwdMovement = characterController.isGrounded == true ? transform.forward * vInput : Vector3.zero;
+            Vector3 rightMovement = characterController.isGrounded == true ? transform.right * hInput : Vector3.zero;
+
+            float _speed = Input.GetButton(RunInput) ? runSpeed : walkSpeed;
+                characterController.SimpleMove(Vector3.ClampMagnitude(fwdMovement + rightMovement, 1f) * _speed);
+
+            if(characterController.isGrounded) {
+                Jump();
+            }
+
+            //Managing Player States
+            if(characterController.isGrounded) {
+                if(didJump) {
+                    playSound(landingSound);
+                    didJump = false;
+                }
+
+                if(hInput == 0 && vInput == 0) {
+                    playerStates = PlayerStates.Idle;
+                    stepCycle = 0;
+                    stepCycleSize = 0;
+                    nextStepIncrement = 60;
+                    nextStep = nextStepIncrement;
+                    incrementStepCycle = false;
+                } else {
+                    if(_speed == walkSpeed) {
+                        playerStates = PlayerStates.Walking;
+                        stepCycleSize = 2;
+                        incrementStepCycle = true;
+                    } else {
+                        playerStates = PlayerStates.Running;
+                        stepCycleSize = 4;
+                        incrementStepCycle = true;
+                    }
+                }
+            } else {
+                playerStates = PlayerStates.Jumping;
+                stepCycle = 0;
+                stepCycleSize = 0;
+                nextStepIncrement = 60;
+                nextStep = nextStepIncrement;
+                incrementStepCycle = false;
+            }
+        }
+
+        void Jump() {
+            if(Input.GetButtonDown(JumpInput)) {
+                StartCoroutine(PerformJumpRoutine());
+                didJump = true;
+            }
+        }
+
+        IEnumerator PerformJumpRoutine() {
+            float _jump = JumpForce;
+
+            do {
+                characterController.Move(Vector3.up * _jump * Time.deltaTime);
+                _jump -= Time.deltaTime;
+                yield return null;
+            }
+            while(!characterController.isGrounded);
+        }
+
+        private void playSound(AudioClip sound) {
+            audioSource.clip = sound;
+            audioSource.Play();
+        }
+
+    }
+}
